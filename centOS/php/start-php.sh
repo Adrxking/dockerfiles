fa306478410affcda509530fd19715b42d2ae35c
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-11-24 13:15:15 +0100

    php centos image changed

--- centOS/php/start-php.sh
+++ centOS/php/start-php.sh
@@ -4,7 +4,27 @@ bash /root/start.sh
 
 set -e
 
-yum update \
-&& yum install -y nginx \
-&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-&& echo "daemon off;" >> /etc/nginx/nginx.conf
+webPath=/www/myapp
+
+yum update -y && yum install -y
+
+yum install -y \\
+php56 \\
+php-pear \\
+php56-cli \\
+php56-common \\
+php56-fpm \\
+php56-soap \\
+php56-pecl-redis 
+
+sed -e 's/127.0.0.1:9000/9000/' \\
+    -e '/allowed_clients/d' \\
+    -e '/catch_workers_output/s/^;//' \\
+    -e '/error_log/d' \\
+    -i /etc/php-fpm.d/www.conf 
+
+mkdir -p $webPath
+
+cd $webPath
+
+tail -f /dev/null
