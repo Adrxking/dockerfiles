f61511f9008b59bce93ef225ef48b82fd8272aa5
Adrianas Vitys <75079985+Adrxking@users.noreply.github.com>
2021-11-30 19:03:03 +0100

    mysql finished and debugged

--- centOS/mysql/start-mysql.sh
+++ centOS/mysql/start-mysql.sh
@@ -2,21 +2,56 @@
 
 bash /root/start.sh
 
+# Eliminar la carpeta si auto.cnf no existe para generar una BD nueva
+if ! [ -f /var/lib/mysql/auto.cnf ]
+ then
+    rm -rf /var/lib/mysql
+    mkdir /var/lib/mysql
+fi
+
+# Conceder permisos a nginx en la carpeta
 chown -R mysql:mysql /var/lib/mysql
 chmod 0755 /var/lib/mysql
 
 chown root:mysql /usr/sbin/mysqld
 chmod 0770 /usr/sbin/mysqld
 
-mkdir /var/run/mysqld
-touch /var/run/mysqld/mysqld.pid
-chown -R mysql:mysql /var/run/mysqld
+# Crear la siguiente carpeta si no existe para el socket y el PID
+if ! [ -d /var/run/mysqld ]
+ then
+    mkdir /var/run/mysqld
+fi
 
-cd /var/lib/mysql
+# Crear el PID y conceder permisos a mysql si no existe
+if ! [ -f /var/run/mysqld/mysqld.pid ]
+ then
+    touch /var/run/mysqld/mysqld.pid
+    chown -R mysql:mysql /var/run/mysqld
+fi
 
-mysqld --initialize-insecure
+# Comprobar que no existe una base de datos inicializada actualmente
+if ! [ -f /var/lib/mysql/auto.cnf ]
+ then
+    mysqld --initialize-insecure
+fi
 
 mysqld -D
+
+# Si no es la primera vez que se ejecuta el expect lo ejecutamos, creamos un archivo de que lo hemos ejecutado y hacemos la configuración
+# por primera vez y la creación del USUARIO 
+if ! [ -f /var/lib/mysql/notFirst.txt ]
+ then
+    expect /root/script.exp
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWD}';"          >  temporal.sql
+    echo "CREATE USER '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWD}';"   >> temporal.sql
+    echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER_NAME}'@'%' WITH GRANT OPTION;"   >> temporal.sql
+    echo "FLUSH PRIVILEGES;"                                                            >> temporal.sql
+    echo "CREATE DATABASE ${MYSQL_DATABASE_NAME};"                                      >> temporal.sql
+    mysql -u root -p"temporal" < temporal.sql
+    rm temporal.sql
+    # Crear el archivo not first para dejar constancia de que no es la primera vez que se ejecuta el expect
+    touch /var/lib/mysql/notFirst.txt
+fi
 
 echo "Imagen MySQL terminada"
 
