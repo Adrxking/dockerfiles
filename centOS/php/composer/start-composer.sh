755da78c1bd02b6bb63e3e2c5eca08e5ac4366bc
aAdrianas Vitys <adrianalbujaira@gmail.com>
2021-12-02 22:56:04 +0100

    composer debug image

--- centOS/php/composer/start-composer.sh
+++ centOS/php/composer/start-composer.sh
@@ -2,10 +2,6 @@
 
 bash /root/start-php.sh
 
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-
-chmod +x /usr/local/bin/composer
-
 if ! [ -f /root/laravel.txt ]
  then
     php-fpm -F -R
